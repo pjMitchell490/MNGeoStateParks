7252e144acca2b3bc8d1c4e90973dc286a78b399
pjMitchell490 <pmitchell490@icloud.com>
2025-04-08 18:54:33 -0500

    Got Minnesota clip working, added some section headers

--- index.js
+++ index.js
@@ -1,22 +1,33 @@
 import { minnesota } from './MN_Boundary.js';
 
 
-var map = L.map('map').setView([45.4, -95.4], 5.25);
+var map = L.map('map').setView([46.5, -93.5], 6);
 
 var USGS_USImagery = L.tileLayer('https://basemap.nationalmap.gov/arcgis/rest/services/USGSImageryOnly/MapServer/tile/{z}/{y}/{x}', {
-    minZoom: 4,
-	maxZoom: 20,
-	attribution: 'Tiles courtesy of the <a href="https://usgs.gov/">U.S. Geological Survey</a>'
+    minZoom: 6,
+    maxZoom: 20,
+    attribution: 'Tiles courtesy of the <a href="https://usgs.gov/">U.S. Geological Survey</a>'
 });
 
-var outside_bounds = [[0, -160], [0, 160], [80, 160], [80, -160]];
+var outside_bounds = [[-90, -180], [-90, 180], [90, 180], [90, -180]];
 
 var mn_bounds = minnesota.features[0].geometry.coordinates[0];
 
-var mn_mask = L.polygon([outside_bounds, mn_bounds], {color: 'black'});
-console.log(mn_bounds[0]);
+for(let i = 0; i < mn_bounds.length; i++) {
+    mn_bounds[i].reverse();
+}
+
+var mn_mask = L.polygon([outside_bounds, mn_bounds], {color: 'black', stroke: false, fillOpacity: 0.50});
+
+var stateParks = [
+    L.marker([44.866, -93.191], {title: 'Fort Snelling State Park'}),
+    L.marker([45.4, -92.651], {title: 'Interstate State Park'}),
+    L.marker([44.148, -94.095], {title: 'Minneopa State Park'})
+];
+
 USGS_USImagery.addTo(map);
-let mn = L.polygon(mn_bounds, {color: 'red'});
-//mn_mask.addTo(map);
-mn.addTo(map);
-L.geoJSON(minnesota).addTo(map);
+mn_mask.addTo(map);
+
+for(let i = 0; i < stateParks.length; i++) {
+    stateParks[i].addTo(map);
+}
